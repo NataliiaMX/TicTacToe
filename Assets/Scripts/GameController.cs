e0b59dfcb52c8d1e3f487da54a4d6bc3c1be6c04
Nataliia <natalia.egorova978@gmail.com>
2022-10-09 11:35:43 +0300

    Started setting up game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int whosTurn; //0 for x, 1 for 0
+    public int turnCount;
+    public GameObject[] _turnIcons;
+    public Sprite[] _playerIcons; 
+    public Button[] _ticTacToeSpaces; //playable spaces 
+
+    private void Start() 
     {
-        
+        GameSetup();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GameSetup()
     {
-        
+        whosTurn = 0;
+        turnCount = 0;
+        _turnIcons[0].SetActive(true);
+        _turnIcons[1].SetActive(false);
+
+        for(int i = 0; i < _ticTacToeSpaces.Length; i++)
+        {
+            _ticTacToeSpaces[i].interactable = true;
+            _ticTacToeSpaces[i].GetComponent<Image>().sprite = null;
+        }
     }
 }
